293bf2937c227a92b22969b0479cb59f6d770847
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-30 19:43:48 -0300

    altura e largura randomicas

---
+++ Docs/engine/jogo.js
@@ -0,0 +1,13 @@
+
+// Pegando a altura e largura da página
+
+var altura = 0;
+var largura = 0;
+function ajusta_tamanho_palco_jogo(){
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(altura,largura)
+}
+
+ajusta_tamanho_palco_jogo()
+
