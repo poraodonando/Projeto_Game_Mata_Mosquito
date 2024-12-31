de34ce23e5bf16ff4f34d69588ff4f7b19ece7e0
Nando Batista <65638072+poraodonando@users.noreply.github.com>
2024-12-30 23:20:46 -0300

    adição do personagem

--- Docs/engine/jogo.js
+++ Docs/engine/jogo.js
@@ -6,8 +6,83 @@ var largura = 0;
 function ajusta_tamanho_palco_jogo(){
     altura = window.innerHeight;
     largura = window.innerWidth;
-    console.log(altura,largura)
+    console.log(largura,altura)
 }
 
 ajusta_tamanho_palco_jogo()
+
+
+function posicao_aleatoria(){
+
+    //Removendo mosquito anterior(caso exista)
+    
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+
+
+
+
+    // Criando uma posição aleatória para o mosquito
+    
+    var posicao_X = Math.floor(Math.random()*largura) -90;
+    var posicao_Y = Math.floor(Math.random()*altura) -90;
+
+    posicao_X = posicao_X < 0 ? posicao_X = 0: posicao_X 
+    posicao_Y = posicao_Y < 0 ? posicao_Y = 0: posicao_Y 
+  
+    
+    //Criar o elemento HTML
+    
+    var mosquito = document.createElement('img');
+    mosquito.src = 'Assets/Imagens/mosquito.png';
+    mosquito.className = tamanho_aleatorio() + ' ' +lado_aleatorio()
+    mosquito.style.left =  posicao_X +'px';
+    mosquito.style.top = posicao_Y +'px';
+    mosquito.style.position = 'absolute';
+    mosquito.id ='mosquito'
+    
+
+  
+    document.body.appendChild(mosquito)
+
+    console.log(lado_aleatorio());
+    
+}
+
+
+//Criando Mosquitos com tamanhos variados
+
+function tamanho_aleatorio(){
+    var classe = Math.floor(Math.random()*3)
+    
+    switch(classe){
+        case 0:
+            return 'mosquito_1'
+        case 1:
+            return 'mosquito_2'
+        case 2:
+            return 'mosquito_3'
+
+    }
+}
+
+//Criando lados aleatórios
+
+function lado_aleatorio(){
+    var classe = Math.floor(Math.random()*4)
+    
+    switch(classe){
+        case 0:
+            return 'lado_A'
+        case 1:
+            return 'lado_B'
+        case 2:
+            return 'lado_C'
+        case 3:
+            return 'lado_D'
+    }
+}
+
+
 
